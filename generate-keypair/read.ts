756b086f88077dc3bcc4936147ddd559b970ac72
Poulav Bhowmick <bpoulav@gmail.com>
2024-05-19 00:45:07 +0530

    read from solana

---
+++ generate-keypair/read.ts
@@ -0,0 +1,9 @@
+import { Connection, PublicKey, clusterApiUrl, LAMPORTS_PER_SOL } from "@solana/web3.js";
+
+const connection = new Connection(clusterApiUrl("devnet"));
+const address = new PublicKey('6RdvAYjB3qN9m4g9KmRh7sz6tpsjUrtzh3Jhbi6MXGHH');
+const balance = await connection.getBalance(address);
+const balanceInSol = balance / LAMPORTS_PER_SOL;
+
+console.log(`The balance of the account at ${address} is ${balanceInSol} SOL`); 
+console.log(`✅ Finished!`)
